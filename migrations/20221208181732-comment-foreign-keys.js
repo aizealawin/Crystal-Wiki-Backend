f274feb6cdf68e8aa583142241362e798cd3ea93
aizealawin <ktmudalige@gmail.com>
2022-12-08 13:19:44 -0500

    set up tables and table attributes

---
+++ migrations/20221208181732-comment-foreign-keys.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('comments', 'articleId', {
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'articles',
+        key: 'id'
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('comments', 'articleId', {
+      type: Sequelize.INTEGER
+    })
+  }
+}
---
+++ models/comment.js
@@ -0,0 +1,42 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Comment.belongsToMany(models.User, { foreignKey: 'userId' })
+      Comment.belongsToMany(models.Article, { foreignKey: 'articleId' })
+    }
+  }
+  Comment.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      articleId: {
+        type: DataTypes.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'articles',
+          key: 'id'
+        }
+      },
+      content: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'Comment',
+      tableName: 'comments'
+    }
+  )
+  return Comment
+}
