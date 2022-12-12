39cd9a0e00c247d68417c6d919756a39b7431529
aizealawin <ktmudalige@gmail.com>
2022-12-12 14:44:13 -0500

    Deployment 6

--- config/config.js
+++ config/config.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 module.exports = {
   development: {
-    database: 'crystal_wiki_development',
-    dialect: 'postgres'
+    database: 'DATABASE_URL',
+    dialect: 'postgres',
+    host: 'containers-us-west-152.railway.app',
+    username: 'postgres',
+    password: 'I9Dzg7fe6nxJSqwZk7RN',
+    port: 7498
   },
   test: {
     database: 'crystal_wiki_test',
