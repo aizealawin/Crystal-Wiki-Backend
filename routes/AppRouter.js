f20ec5bc92b111ae9ca5f66b160780ec4ef2a379
aizealawin <ktmudalige@gmail.com>
2022-12-14 22:01:17 -0500

    auth router init

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,12 +2,11 @@ const Router = require('express').Router()
 const ArticleRouter = require('./ArticleRouter')
 const UserRouter = require('./UserRouter')
 const CommentRouter = require('./CommentRouter')
-// const UserRouter = require('./UserRouter')
-// const AuthRouter = require('./AuthRouter')
+const AuthRouter = require('./AuthRouter')
 
 Router.use('/articles', ArticleRouter)
 Router.use('/users', UserRouter)
 Router.use('/comments', CommentRouter)
-// Router.use('/auth', AuthRouter)
+Router.use('/auth', AuthRouter)
 
 module.exports = Router
